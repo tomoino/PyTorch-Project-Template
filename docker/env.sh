5e7cf5ccd38d3d2c692992d9ae62bf62842a9659
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2021-08-29 16:26:07 +0900

    [add] add notebooks

--- docker/env.sh
+++ docker/env.sh
@@ -6,6 +6,8 @@ export IMAGE_NAME=${USER}_pytorch_project
 export CONTAINER_NAME=${USER}_pytorch_project
 export MLFLOW_HOST_PORT=5000
 export MLFLOW_CONTAINER_PORT=5000
+export JUPYTER_HOST_PORT=18885
+export JUPYTER_CONTAINER_PORT=18885
 
 if [ -e docker/env_dev.sh ]; then
   . docker/env_dev.sh
